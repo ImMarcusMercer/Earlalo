341de1d3e501c512ec16dd052eaf49a73a61668b
ImMarcusMercer <immarcusmercer@gmail.com>
2025-03-06 21:38:00 +0800

    first

---
+++ Accounts/Account.java
@@ -0,0 +1,12 @@
+package Accounts;
+
+import java.util.ArrayList;
+import Bank.Bank;
+public class Account {
+    String ACCOUNTNUMBER, OWNERFNAME, OWNERLNAME, OWNEREMAIL;
+    String pin;
+    ArrayList <Transaction> TRANSACTIONS= new ArrayList<>();
+
+
+    
+}
